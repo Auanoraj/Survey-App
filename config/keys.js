a410530829cff04d93492f3e14dfac856214461c
auanoraj <auanoraj@live.com>
2018-08-13 12:41:52 +0530

    Third commit

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: '1090700438381-svrt2p32gj8ile820ad2t6v83gd4c933.apps.googleusercontent.com',
+    googleClientSecret: 'FeZ4gtAQYf_M3mzRktHPtWJD',
+    mongoURI: 'mongodb://rakshith:rakshith23@ds219432.mlab.com:19432/survey-app',
+    cookieKey: 'secret123'
+};
